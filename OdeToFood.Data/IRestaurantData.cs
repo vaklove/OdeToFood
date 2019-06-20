5084d8b8f16739675652278f41068d3ef675eea2
Vikas Shah <vikas.shah@essilorusa.com>
2019-06-20 13:45:20 -0400

    added model binding with property and tag helper

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,33 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id=1,Name="Scott pizza",Location="Marryland",Cuisine=CuisineType.Italian },
+                new Restaurant { Id=1,Name="Chipolate",Location="New Jersey",Cuisine=CuisineType.Mexican },
+                new Restaurant { Id=1,Name="The India",Location="New York",Cuisine=CuisineType.Indian },
+            };
+        }
+        public IEnumerable<Restaurant> GetRestaurantsByName( string name=null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration configration;
+        private readonly IRestaurantData restaurantData;
+
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        [BindProperty(SupportsGet =true)]
+        public string SearchTerm { get; set; }
+
+
+        public ListModel(IConfiguration configration, IRestaurantData restaurantData)
+        {
+            this.configration = configration;
+            this.restaurantData = restaurantData;
+        }
+        public void OnGet()
+        {
+            Message = configration["Message"];
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
+        }
+    }
+}
